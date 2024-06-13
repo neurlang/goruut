669d75e340e9f7ffc4723c944649c8a534e55f4e
neurlang <77860779+neurlang@users.noreply.github.com>
2024-06-13 23:14:27 +0200

    add english

--- dicts/dicts.go
+++ dicts/dicts.go
@@ -3,6 +3,7 @@ package dicts
 import "github.com/neurlang/goruut/dicts/czech"
 import "github.com/neurlang/goruut/dicts/slovak"
 import "github.com/neurlang/goruut/dicts/arabic"
+import "github.com/neurlang/goruut/dicts/english"
 import "errors"
 
 var ErrUnsupportedLanguage = errors.New("unsupported")
@@ -21,6 +22,8 @@ func GetDict(lang, filename string) ([]byte, error) {
 		return slovak.Language.ReadFile(filename)
 	case "Arabic":
 		return arabic.Language.ReadFile(filename)
+	case "English":
+		return english.Language.ReadFile(filename)
 	default:
 		return nil, ErrUnsupportedLanguage
 	}
