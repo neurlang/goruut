cc38b6bc8a4247e2ade36b9c271aedf62dbf6d10
neurlang <77860779+neurlang@users.noreply.github.com>
2024-06-19 18:49:44 +0200

    ipa flavor replace start by longest

--- repo/services/ipa_flavor_service.go
+++ repo/services/ipa_flavor_service.go
@@ -13,27 +13,43 @@ type IIpaFlavorService interface {
 
 type IpaFlavorService struct {
 	mapping *map[string]map[string]string
+	longest *map[string]int
 }
 
 func (p *IpaFlavorService) Apply(lang, word string) (ret string) {
 
-	for k, v := range (*p.mapping)[lang] {
-		if k == v {
-			log.Now().Errorf("Ipa flavour %s does have identical source and dest string: %s to %s", lang, k, v)
+	for i := (*p.longest)[lang]; i > 0; i-- {
+		for k, v := range (*p.mapping)[lang] {
+			if len(k) != i {
+				continue
+			}
+
+			if k == v {
+				log.Now().Errorf("Ipa flavour %s does have identical source and dest string: %s to %s", lang, k, v)
+			}
+
+			word = strings.ReplaceAll(word, k, v)
 		}
-
-		word = strings.ReplaceAll(word, k, v)
 	}
-
 	return word
 }
 
 func NewIpaFlavorService(di *DependencyInjection) *IpaFlavorService {
 
 	mapping := MustAny[interfaces.IpaFlavor](di).GetIpaFlavors()
+	longest := make(map[string]int)
+
+	for lang, dict := range mapping {
+		for k := range dict {
+			if len(k) > longest[lang] {
+				longest[lang] = len(k)
+			}
+		}
+	}
 
 	return &IpaFlavorService{
 		mapping: &mapping,
+		longest: &longest,
 	}
 }
 
