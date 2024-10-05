5197ab1b29afb5f65865bb7c8449d026bae57e94
neurlang <77860779+neurlang@users.noreply.github.com>
2024-10-05 20:49:22 +0200

    max target in clean

--- cmd/analysis/clean_language.sh
+++ cmd/analysis/clean_language.sh
@@ -5,5 +5,5 @@ analysis_script="./analysis"
 original_json="../../dicts/$1/language.json"
 srcfile="../../dicts/$1/dirty.tsv"
 dstfile="../../dicts/$1/clean.tsv"
-$analysis_script --lang "$original_json" --srcfile "$srcfile" --dstfile "$dstfile" -loss -nospaced -noipadash $2 $3 $4 $5 $6 $7 $8 $9
+$analysis_script --target 9999999999999 --lang "$original_json" --srcfile "$srcfile" --dstfile "$dstfile" -loss -nospaced -noipadash $2 $3 $4 $5 $6 $7 $8 $9
 
