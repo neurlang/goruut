3d938fb114562fec6b425f1b8cdc7caaae731bf3
neurlang <77860779+neurlang@users.noreply.github.com>
2024-10-10 19:19:39 +0200

    fix wrong number in study language and phases

--- cmd/analysis/study_language.sh
+++ cmd/analysis/study_language.sh
@@ -4,20 +4,21 @@
 #phases_array=( 12 7 4 2 1 0 )
 srcfile="../../dicts/$1/dirty.tsv"
 file_size=$(cat $srcfile | wc -l)
-phases_array=( 0 )
-
+phases_array=()
 # more phases for bigger file. The phases are fibbonacci - 1
-(( file_size > 12200 )) && phases_array=( 12 "${phases_array[@]}" )
-(( file_size > 90200 )) && phases_array=( 7 "${phases_array[@]}" )
-(( file_size > 60200 )) && phases_array=( 4 "${phases_array[@]}" )
-(( file_size > 30200 )) && phases_array=( 2 "${phases_array[@]}" )
-(( file_size > 200 ))   && phases_array=( 1 "${phases_array[@]}" )
-
+(( file_size > 150200 )) && phases_array=( "${phases_array[@]}" 20 )
+(( file_size > 120200 )) && phases_array=( "${phases_array[@]}" 12 )
+(( file_size >  90200 )) && phases_array=( "${phases_array[@]}"  7 )
+(( file_size >  60200 )) && phases_array=( "${phases_array[@]}"  4 )
+(( file_size >  30200 )) && phases_array=( "${phases_array[@]}"  2 )
+(( file_size >    200 )) && phases_array=( "${phases_array[@]}"  1 )
+phases_array=( "${phases_array[@]}" 0 )
 
 for i in {0..100}; do 
 	# Loop through each phase in the array
 	for phase in "${phases_array[@]}"
 	do
+		echo "Phase $phase"
 		./creator.sh $i $1 --randsubs $phase $2 $3 $4 $5 $6 ; 
 		if [[ "$?" -eq 1 ]]; then
 		    echo "Target met. Exiting."
