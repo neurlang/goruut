cfe07019a29ddb59b9a0439beec0f09bbfcba7ce
neurlang <77860779+neurlang@users.noreply.github.com>
2025-02-08 21:41:01 +0100

    add new data for langs

--- dicts/hausa/language.go
+++ dicts/hausa/language.go
@@ -2,5 +2,5 @@ package hausa
 
 import "embed"
 
-//go:embed *.tsv language.json weights1.json.lzw language_reverse.json weights1_reverse.json.zlib
+//go:embed *.tsv language.json weights1.json.zlib language_reverse.json weights1_reverse.json.zlib
 var Language embed.FS
