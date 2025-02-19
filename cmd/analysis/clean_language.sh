b361df2d97d0ac1744b7525ab4f54f663ce81189
neurlang <77860779+neurlang@users.noreply.github.com>
2025-02-19 21:07:01 +0100

    use the new analysis2 for cleaning from now on.

--- cmd/analysis/clean_language.sh
+++ cmd/analysis/clean_language.sh
@@ -9,11 +9,11 @@ for arg in "$@"; do
   fi
 done
 
-analysis_script="./analysis"
+analysis_script="../analysis2/analysis2"
 dicttomap_script="../dicttomap/dicttomap"
 
 original_json="../../dicts/$1/language$reverse_flag.json"
 srcfile="../../dicts/$1/dirty.tsv"
 dstfile="../../dicts/$1/clean$reverse_flag.tsv"
-$analysis_script --target 9999999999999 --lang "$original_json" --srcfile "$srcfile" --dstfile "$dstfile" -loss -nospaced -noipadash $2 $3 $4 $5 $6 $7 $8 $9
+$analysis_script --lang "$original_json" --srcfile "$srcfile" --dstfile "$dstfile" $2 $3 $4 $5 $6 $7 $8 $9
 $dicttomap_script --srcfile "$dstfile" --lang "$original_json" --writeback
