4d7b52383232f6688a15d92bdded40a42bf36f72
neurlang <77860779+neurlang@users.noreply.github.com>
2025-02-28 16:05:46 +0100

    add the new learn autoregressive scripts

---
+++ cmd/analysis2/learn_language.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Function to handle the SIGINT signal (Ctrl+C)
+cleanup() {
+    echo "Caught SIGINT, killing both processes..."
+    kill -SIGTERM $PID1 $PID2
+    exit 1
+}
+
+# Trap the SIGINT signal and call the cleanup function
+trap cleanup SIGINT
+
+#train
+
+../../../classifier/cmd/train_phonemizer2/train_phonemizer2 \
+--langjson ../../dicts/$1/language.json \
+--dirtytsv ../../dicts/$1/dirty.tsv \
+--learntsv ../../dicts/$1/learn.tsv \
+--dstmodel ../../dicts/$1/weights3.json.zlib $2 $3 $4 $5 $6 & # > /dev/null 2>&1 &
+PID1=$!
+
+# Start the second process in the background
+../backtest/backtest -testing  -langname $1 &
+PID2=$!
+
+# Wait for both processes to finish
+wait $PID1
+cleanup
---
+++ cmd/analysis2/learn_language_reverse.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to handle the SIGINT signal (Ctrl+C)
+cleanup() {
+    echo "Caught SIGINT, killing both processes..."
+    kill -SIGTERM $PID1 $PID2
+    exit 1
+}
+
+# Trap the SIGINT signal and call the cleanup function
+trap cleanup SIGINT
+
+#train
+
+../../../classifier/cmd/train_phonemizer2/train_phonemizer2 \
+--reverse \
+--langjson ../../dicts/$1/language_reverse.json \
+--dirtytsv ../../dicts/$1/dirty.tsv \
+--learntsv ../../dicts/$1/learn_reverse.tsv \
+--dstmodel ../../dicts/$1/weights3_reverse.json.zlib $2 $3 $4 $5 $6 & # > /dev/null 2>&1 &
+PID1=$!
+
+# Start the second process in the background
+../backtest/backtest -testing --reverse -langname $1 &
+PID2=$!
+
+# Wait for both processes to finish
+wait $PID1
+cleanup
