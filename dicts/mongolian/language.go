5e765d846ce071048bd896f8148c0bdceb66e168
neurlang <77860779+neurlang@users.noreply.github.com>
2025-02-28 21:58:17 +0100

    commit languages

--- dicts/mongolian/language.go
+++ dicts/mongolian/language.go
@@ -2,5 +2,5 @@ package mongolian
 
 import "embed"
 
-//go:embed *.tsv language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib
+//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib
 var Language embed.FS
