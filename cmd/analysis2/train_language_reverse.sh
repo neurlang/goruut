1d40cbc0e0d1f4ddea84e65e4101bf9245633f5f
neurlang <77860779+neurlang@users.noreply.github.com>
2025-03-16 12:41:54 +0100

    remove old analysis aligner

--- cmd/analysis2/train_language_reverse.sh
+++ cmd/analysis2/train_language_reverse.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-../analysis/train_language_reverse.sh $1 -minpremodulo 1000 -maxdepth 9999 $2 $3 $4 $5 $6 $7 $8 $9
+# Function to handle the SIGINT signal (Ctrl+C)
+cleanup() {
+    echo "Caught SIGINT, killing both processes..."
+    kill -SIGTERM $PID1 $PID2
+    exit 1
+}
+
+# Trap the SIGINT signal and call the cleanup function
+trap cleanup SIGINT
+
+#train
+
+../../../classifier/cmd/train_phonemizer/train_phonemizer \
+--cleantsv ../../dicts/$1/clean_reverse.tsv \
+--dstmodel ../../dicts/$1/weights1_reverse.json.zlib $2 $3 $4 $5 $6 $7 $8 $9 & # > /dev/null 2>&1 &
+PID1=$!
+
+# Start the second process in the background
+../backtest/backtest -reverse -testing  -langname $1 &
+PID2=$!
+
+# Wait for both processes to finish
+wait $PID1
+cleanup
