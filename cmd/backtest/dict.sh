6062fe3626483ab48eb165882f878424b3dc7e64
neurlang <77860779+neurlang@users.noreply.github.com>
2025-08-01 22:36:38 +0200

    Load abbr.tsv to dict

--- cmd/backtest/dict.sh
+++ cmd/backtest/dict.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+EXTRAFILE=''
+if test -f ../../dicts/$1/abbr.tsv; then
+  sed  -e 's/"/""/g' -e 's/\[/"\[/g' -e 's/\]/\]"/g' "../../dicts/$1/abbr.tsv" > "../../dicts/$1/abbr.dq.tsv"
+  EXTRAFILE="../../dicts/$1/abbr.dq.tsv"
+fi
+
 ./learn.sh $1
-cat ../../dicts/$1/learn_reverse.tsv ../../dicts/$1/learn.tsv | sort | uniq > ../../dicts/$1/missing.all.tsv
+
+
+
+cat ../../dicts/$1/learn_reverse.tsv ../../dicts/$1/learn.tsv $EXTRAFILE | sort | uniq > ../../dicts/$1/missing.all.tsv
 ./backtest --dumpcompress --langname $1
 rm ../../dicts/$1/missing.all.tsv
+
+if test -f  "$EXTRAFILE"; then
+  rm $EXTRAFILE
+fi
