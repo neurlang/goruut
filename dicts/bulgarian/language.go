069ad697e0413243ed92e0c94915520df70d9858
neurlang <77860779+neurlang@users.noreply.github.com>
2026-02-11 22:06:09 +0100

    Demo Bulgarian Model (preview)

--- dicts/bulgarian/language.go
+++ dicts/bulgarian/language.go
@@ -2,5 +2,5 @@ package bulgarian
 
 import "embed"
 
-//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib
+//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib weights*.bin.zlib
 var Language embed.FS
