815ad1bc1d1ab2fe839c71bb3ac93551b690d3fa
neurlang <77860779+neurlang@users.noreply.github.com>
2026-05-03 11:18:05 +0200

    Load the new reverse models

--- dicts/japanese/language.go
+++ dicts/japanese/language.go
@@ -2,5 +2,5 @@ package japanese
 
 import "embed"
 
-//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib
+//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*.bin.zlib
 var Language embed.FS
--- dicts/maltese/language.go
+++ dicts/maltese/language.go
@@ -2,5 +2,5 @@ package maltese
 
 import "embed"
 
-//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*_reverse.json.zlib
+//go:embed missing* language.json weights*.json.zlib language_reverse.json weights*.bin.zlib
 var Language embed.FS
